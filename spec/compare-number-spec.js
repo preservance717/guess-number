38a6f6a9c02c79c701e5ec87ef0ca4d281f11f31
preservance717 <GL717@outlook.com>
2016-07-26 22:01:22 +0800

    add class CompareNumber test

---
+++ spec/compare-number-spec.js
@@ -0,0 +1,14 @@
+const CompareNumber = require('../src/compare-number');
+
+describe('compare number', () => {
+    const answer = '1234';
+    let input;
+
+    it('correct digits and correct position', ()=>{
+        input = '1234';
+        const text = CompareNumber.compare(input, answer);
+        const expectText = `4A0B`; 
+        
+        expect(text).toEqual(expectText);
+    })
+});
