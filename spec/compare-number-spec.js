405723c51023761ddd5d4fc540f51959d2417b81
preservance717 <GL717@outlook.com>
2016-07-26 22:06:02 +0800

    add CompareNumber test when correct digits and wrong position

--- spec/compare-number-spec.js
+++ spec/compare-number-spec.js
@@ -10,5 +10,13 @@ describe('compare number', () => {
         const expectText = `4A0B`; 
         
         expect(text).toEqual(expectText);
-    })
+    });
+
+    it('correct digits and correct position', ()=>{
+        input = '5678';
+        const text = CompareNumber.compare(input, answer);
+        const expectText = `0A0B`;
+
+        expect(text).toEqual(expectText);
+    });
 });
