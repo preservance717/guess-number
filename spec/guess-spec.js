a63fa425cae9f5c9686d4813df5cf39484371cfc
preservance717 <GL717@outlook.com>
2016-07-26 23:00:49 +0800

    create guess test and achieve

--- spec/guess-spec.js
+++ spec/guess-spec.js
@@ -1,3 +1,10 @@
-/**
- * Created by gaole on 7/26/16.
- */
+const Guess = require('../src/guess');
+const AnswerGenerator = require('../src/answer-generator');
+
+describe('Guess', () => {
+    it('should print the game', () => {
+        spyOn(AnswerGenerator, 'generator').and.returnValue('1234');
+        const result = Guess.guess('1234');
+        expect(result).toEqual('4A0B');
+    })
+});
--- src/guess.js
+++ src/guess.js
@@ -1,3 +1,11 @@
-/**
- * Created by gaole on 7/26/16.
- */
+const AnswerGenerator = require('./answer-generator');
+const CompareNumber = require('./compare-number');
+
+class Guess{
+    static guess(input){
+        const  answer = AnswerGenerator.generator();
+        return CompareNumber.compare(input, answer);
+    }
+}
+
+module.exports = Guess;
